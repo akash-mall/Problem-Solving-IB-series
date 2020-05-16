ed567d4c1801c844cb7e10b40a7cebe0907cdafe
Akash Mall <64031858+akash-mall@users.noreply.github.com>
2020-05-17 01:26:22 +0530

    added permutation solution to grid Unique paths

--- gridUniquePaths.cpp
+++ gridUniquePaths.cpp
@@ -15,3 +15,26 @@ int Solution::uniquePaths(int A, int B) {
     utility(0,0,A,B,cnt);
     return cnt;
 }
+
+//********************************************************************************
+
+//Solution 2 using  combinatrics
+
+long int fact(long int a)
+{
+    if(a<=1) return 1;
+    return fact(a-1)*a;
+}
+
+
+int Solution::uniquePaths(int A, int B) {
+    long int cnt = 0;
+    long m = max(A,B),f=1;
+    long minm = min(A,B);
+    for(long i = (A+B-2);i>m-1;--i)
+    {
+        f *= (i);
+    }
+    cnt = f/fact(minm-1);
+    return (int)cnt;
+}
